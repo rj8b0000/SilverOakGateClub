53d35305070f20ebc96aab6291bb581d89fa47bf
rj8b0000 <100023483+rj8b0000@users.noreply.github.com>
2026-03-20 11:10:38 +0530

    Third commit

---
+++ SilverOakGateClub/Migrations/20260320051742_AllBranchesSupport.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SilverOakGateClub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllBranchesSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lectures_Branches_BranchId",
+                table: "Lectures");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MockTests_Branches_BranchId",
+                table: "MockTests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Branches_BranchId",
+                table: "Notes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BranchId",
+                table: "Notes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BranchId",
+                table: "MockTests",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BranchId",
+                table: "Lectures",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lectures_Branches_BranchId",
+                table: "Lectures",
+                column: "BranchId",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MockTests_Branches_BranchId",
+                table: "MockTests",
+                column: "BranchId",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Branches_BranchId",
+                table: "Notes",
+                column: "BranchId",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lectures_Branches_BranchId",
+                table: "Lectures");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MockTests_Branches_BranchId",
+                table: "MockTests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Branches_BranchId",
+                table: "Notes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BranchId",
+                table: "Notes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BranchId",
+                table: "MockTests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BranchId",
+                table: "Lectures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lectures_Branches_BranchId",
+                table: "Lectures",
+                column: "BranchId",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MockTests_Branches_BranchId",
+                table: "MockTests",
+                column: "BranchId",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Branches_BranchId",
+                table: "Notes",
+                column: "BranchId",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
